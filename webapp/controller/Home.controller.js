59db51ac27a8ee1aec75fbe4832ad7351e213fed
guilhermeq-dev <guilherme_queiroz97@outlook.com>
2023-12-04 17:14:39 -0300

    feat: adicionando dados teste para a aplicação

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,14 +1,45 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("com.lab2dev.btpexperience.controller.Home", {
             onInit: function () {
+
+                const companyInfo = [
+                    {
+                        companyID: 10001,
+                        companyName: "LAB2DEV",
+                        corporateCode: "12.345.678/0001-90",
+                        webSite: "www.lab2dev.com",
+                        email: "contato@lab2dev.com",
+                        region: "São Paulo"
+                    },
+                    {
+                        companyID: 10002,
+                        companyName: "LAB2DEV",
+                        corporateCode: "12.345.678/0001-90",
+                        webSite: "www.lab2dev.com",
+                        email: "contato@lab2dev.com",
+                        region: "São Paulo"
+                    },
+                    {
+                        companyID: 10003,
+                        companyName: "LAB2DEV",
+                        corporateCode: "12.345.678/0001-90",
+                        webSite: "www.lab2dev.com",
+                        email: "contato@lab2dev.com",
+                        region: "São Paulo"
+                    },
+                ]
+
+                const oModel = new JSONModel(companyInfo)
+                this.getView().setModel(oModel, "companyInfo")
 
             }
         });

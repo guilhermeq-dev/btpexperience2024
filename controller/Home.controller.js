5e172436cf6acfabaf7e829e51b004e032eade6c
guilhermeq-dev <guilherme_queiroz97@outlook.com>
2023-12-05 20:46:41 -0300

    feat: dialog fragment

--- controller/Home.controller.js
+++ controller/Home.controller.js
@@ -2,22 +2,14 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sap/m/Dialog",
-    "sap/m/Button",
-    "sap/m/Input",
-	"sap/ui/layout/form/ResponsiveGridLayout",
-    "sap/ui/layout/form/Form",
-    "sap/ui/layout/form/FormContainer",
-    "sap/ui/layout/form/FormElement",
-    "sap/m/Label",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
     "sap/ui/core/Fragment"
-    
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel, Dialog, Button, Input, ResponsiveGridLayout, Form, FormContainer, FormElement, Label, Filter, FilterOperator, Fragment) {
+    function (Controller, JSONModel, Dialog, Filter, FilterOperator, Fragment) {
         "use strict";
 
         return Controller.extend("com.lab2dev.btpexperience.controller.Home", {
@@ -77,72 +69,18 @@ sap.ui.define([
                 this.createModal()
             },
 
-            // criação do MODAL Cadastro de empresa
+            // função chamando o Dialog de Cadastro de empresa
             createModal: function () {
+                const viewId = this.getView().getId()
 
-            // Responsividade do layout (falta deixar funcional)
-            // var oLayout = new ResponsiveGridLayout({
-            //     columnsL: 2,
-            //     columnsM: 2,
-            // });
-
-            // // Criação do formulário
-            // var oForm = new Form({
-            //     layout: oLayout,
-            //     formContainers: [
-            //         new FormContainer({
-            //             formElements: [
-            //                 new FormElement({
-            //                     label: new Label({ text: "Razão Social" }),
-            //                     fields: [new Input({ placeholder: 'Texto'})]
-            //                 }),
-            //                 new FormElement({
-            //                     label: new Label({ text: "CNPJ" }),
-            //                     fields: [new Input({ placeholder: 'Texto'})]
-            //                 }),
-            //                 new FormElement({
-            //                     label: new Label({ text: "Site" }),
-            //                     fields: [new Input({ placeholder: 'Texto'})]
-            //                 }),
-            //                 new FormElement({
-            //                     label: new Label({ text: "Pacote de patrocínio" }),
-            //                     fields: [new Input({ placeholder: 'Selecione o pacote inicial'})]
-            //                 }),
-            //                 new FormElement({
-            //                     label: new Label({ text: "Nome Fantasia" }),
-            //                     fields: [new Input({ placeholder: 'Texto'})]
-            //                 }),
-            //                 new FormElement({
-            //                     label: new Label({ text: "Estado" }),
-            //                     fields: [new Input({ placeholder: 'Texto'})]
-            //                 }),
-            //                 new FormElement({
-            //                     label: new Label({ text: "E-mail de contato" }),
-            //                     fields: [new Input({ placeholder: 'Texto'})]
-            //                 }),
-            //                 new FormElement({
-            //                     label: new Label({ text: "Logo (imagem)" }),
-            //                     fields: [new Input({ placeholder: 'Clique e escolha um arquivo de imagem'})]
-            //                 }),
-            //             ]
-            //         })
-            //     ]
-            // });
-            //     // Dialog declarado
-            //     var oDialog = new Dialog({
-            //         title: "Dados da empresa",
-            //         content: [oForm],
-            //         beginButton: new Button({  // botao de confirmação para cadastrar uma nova empresa
-            //             text: "Cadastrar empresa",
-            //             type: "Emphasized",
-            //             press: function () {
-            //                 oDialog.close()
-            //             },
-            //         }),
-            //     })
-            //     oDialog.setContentWidth("400px")
-            //     oDialog.setContentHeight("300px")
-            //     oDialog.open() // funcao chamada para abrir o Modal
+                if (!this.dialog) {
+                    this.dialog = sap.ui.xmlfragment(viewId, "com.lab2dev.btpexperience.view.fragments.Dialog", this)
+                    this.getView().addDependent(this.dialog)
+                }
+                this.dialog.open()
+            },
+            onCloseDialog: function () {
+                this.dialog.close()
             },
             // SearchField adicionado 
             onSearch: function (oEvent) {

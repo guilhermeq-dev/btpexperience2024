15a6635701aeb3b807b66ea3eb44342abe90035f
guilhermeq-dev <guilherme_queiroz97@outlook.com>
2023-12-06 17:39:23 -0300

    fix: atualizando o Dialog

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -2,26 +2,19 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
     "sap/m/Dialog",
-    "sap/m/Button",
-    "sap/m/Input",
-	"sap/ui/layout/form/ResponsiveGridLayout",
-    "sap/ui/layout/form/Form",
-    "sap/ui/layout/form/FormContainer",
-    "sap/ui/layout/form/FormElement",
-    "sap/m/Label",
-    "sap/m/Select",
     "sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
+    "sap/ui/model/FilterOperator",
+    
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel, Dialog, Button, Input, ResponsiveGridLayout, Form, FormContainer, FormElement, Label, Select, Filter, FilterOperator) {
+    function (Controller, JSONModel, Dialog, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("com.lab2dev.btpexperience.controller.Home", {
             onInit: function () {
-                // array de objetos simulando uma lista de empresas
+                // array de objetos simulando uma lista de empresas (não utilizavel no momento)
                 const infos = [
                     {
                         companyID: 10001,
@@ -64,83 +57,31 @@ sap.ui.define([
                         region: "Rio de Janeiro"
                     },
                 ]
-                // cria um modelo JSON com as empresas 
-                const oModel = new JSONModel(infos)
-                // faz a ligação com a ViewXML (Aggregation Binding)
+                // cria um modelo JSON com as empresas (dados importados de um arquivo json localizado na model)
+                const oModel = new JSONModel()
+                oModel.loadData('/model/companys.json')
+                // faz a ligação/seta o model na ViewXML (Aggregation Binding)
                 this.getView().setModel(oModel, "companyInfo")
 
             },
-            // função que chama o modal (botao declarado na view)
+            // função chamando o Dialog de Cadastro de empresa
+            createModal: function () {
+                const viewId = this.getView().getId()
+                // carrega o fragmento dialog
+                if (!this.dialog) {
+                    this.dialog = sap.ui.xmlfragment(viewId, "com.lab2dev.btpexperience.view.fragments.Dialog", this)
+                    this.getView().addDependent(this.dialog)
+                }
+                // Abre o dialog
+                this.dialog.open()
+            },
+            // abre o dialog ao clicar no botão "Adicionar empresa" na View
             openModal: function () {
                 this.createModal()
             },
-
-            // criação do MODAL Cadastro de empresa
-            createModal: function () {
-
-            // Responsividade do layout (falta deixar funcional)
-            var oLayout = new ResponsiveGridLayout({
-                columnsL: 2,
-                columnsM: 2,
-            });
-
-            // Criação do formulário
-            var oForm = new Form({
-                layout: oLayout,
-                formContainers: [
-                    new FormContainer({
-                        formElements: [
-                            new FormElement({
-                                label: new Label({ text: "Razão Social" }),
-                                fields: [new Input({ placeholder: 'Texto'})]
-                            }),
-                            new FormElement({
-                                label: new Label({ text: "CNPJ" }),
-                                fields: [new Input({ placeholder: 'Texto'})]
-                            }),
-                            new FormElement({
-                                label: new Label({ text: "Site" }),
-                                fields: [new Input({ placeholder: 'Texto'})]
-                            }),
-                            new FormElement({
-                                label: new Label({ text: "Pacote de patrocínio" }),
-                                fields: [new Input({ placeholder: 'Selecione o pacote inicial'})]
-                            }),
-                            new FormElement({
-                                label: new Label({ text: "Nome Fantasia" }),
-                                fields: [new Input({ placeholder: 'Texto'})]
-                            }),
-                            new FormElement({
-                                label: new Label({ text: "Estado" }),
-                                fields: [new Input({ placeholder: 'Texto'})]
-                            }),
-                            new FormElement({
-                                label: new Label({ text: "E-mail de contato" }),
-                                fields: [new Input({ placeholder: 'Texto'})]
-                            }),
-                            new FormElement({
-                                label: new Label({ text: "Logo (imagem)" }),
-                                fields: [new Input({ placeholder: 'Clique e escolha um arquivo de imagem'})]
-                            }),
-                        ]
-                    })
-                ]
-            });
-                // Dialog declarado
-                var oDialog = new Dialog({
-                    title: "Dados da empresa",
-                    content: [oForm],
-                    beginButton: new Button({  // botao de confirmação para cadastrar uma nova empresa
-                        text: "Cadastrar empresa",
-                        type: "Emphasized",
-                        press: function () {
-                            oDialog.close()
-                        },
-                    }),
-                })
-                oDialog.setContentWidth("400px")
-                oDialog.setContentHeight("300px")
-                oDialog.open() // funcao chamada para abrir o Modal
+            onCloseDialog: function () {
+                // fecha o dialog ao pressionar o botão "Cadastrar empresa" dentro do dialog
+                this.dialog.close()
             },
             // SearchField adicionado 
             onSearch: function (oEvent) {

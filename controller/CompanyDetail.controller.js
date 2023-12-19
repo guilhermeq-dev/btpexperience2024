4de1afb552f9306e5b144853bf55c3bd82cb3aa2
guilhermeq-dev <guilherme_queiroz97@outlook.com>
2023-12-19 18:09:40 -0300

    Projeto finalizado

---
+++ controller/CompanyDetail.controller.js
@@ -0,0 +1,55 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  function (Controller, JSONModel) {
+    "use strict";
+
+    return Controller.extend(
+      "com.lab2dev.btpexperience.controller.CompanyDetail",
+      {
+        onInit: function () {
+          // inicializa a rota
+          this.oRouter = this.getOwnerComponent().getRouter();
+          // sempre que a rota 'CompanyDetail' for acessada vai rodar a função onRouteMatch
+          this.oRouter
+            .getRoute("CompanyDetail")
+            .attachPatternMatched(this.onRouteMatch, this);
+        },
+        onRouteMatch: function (oEvent) {
+          const oArguments = oEvent.getParameter("arguments")
+          
+          this.getCompanyInfo(oArguments.companyId).then((e) => {
+            const oModel = new JSONModel(e);
+            this.getView().setModel(oModel, "companyDetails");
+          })
+        },
+        getCompanyInfo: async function (companyID) {
+            const oModel = new JSONModel();
+            await oModel.loadData("/model/companys.json");
+            const oData = oModel.getData()
+          
+          return oData.find((el) => el.companyID === Number(companyID))
+        },
+        onBackHome: function () {
+          history.back();
+        },
+        onOpenDialog: function () {
+          if (!this.dialog) {
+            this.dialog = sap.ui.xmlfragment(
+              "com.lab2dev.btpexperience.view.fragments.Dialog",
+              this
+            );
+            this.getView().addDependent(this.dialog);
+          }
+          this.dialog.open();
+        },
+        onSendFormData: function () {
+          this.dialog.close();
+        },
+        onCancelAddForm: function () {
+          this.dialog.close();
+        },
+
+      }
+    );
+  }
+);
--- controller/Home.controller.js
+++ controller/Home.controller.js
@@ -1,101 +1,138 @@
-sap.ui.define([
+sap.ui.define(
+  [
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/m/Dialog",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
-    "sap/ui/core/Fragment"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller, JSONModel, Dialog, Filter, FilterOperator, Fragment) {
-        "use strict";
+    "com/lab2dev/btpexperience/model/formatter",
+  ],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, JSONModel, Filter, FilterOperator, formatter) {
+    "use strict";
+    return Controller.extend("com.lab2dev.btpexperience.controller.Home", {
+      formatter: formatter,
+      onInit: function () {
+        
+        const loadJSON = this.loadData();
 
-        return Controller.extend("com.lab2dev.btpexperience.controller.Home", {
-            onInit: function () {
-                // array de objetos simulando uma lista de empresas
-                const infos = [
-                    {
-                        companyID: 10001,
-                        companyName: "LAB2DEV",
-                        corporateCode: "12.345.678/0001-90",
-                        webSite: "www.lab2dev.com",
-                        email: "contato@lab2dev.com",
-                        region: "São Paulo"
-                    },
-                    {
-                        companyID: 10002,
-                        companyName: "LAB2DEV",
-                        corporateCode: "12.345.678/0001-90",
-                        webSite: "www.lab2dev.com",
-                        email: "contato@lab2dev.com",
-                        region: "São Paulo"
-                    },
-                    {
-                        companyID: 10003,
-                        companyName: "LAB2DEV",
-                        corporateCode: "12.345.678/0001-90",
-                        webSite: "www.lab2dev.com",
-                        email: "contato@lab2dev.com",
-                        region: "São Paulo"
-                    },
-                    {
-                        companyID: 10004,
-                        companyName: "PASTELARIA",
-                        corporateCode: "11.345.678/0001-70",
-                        webSite: "www.pastelaria.com",
-                        email: "contato@pastelaria.com",
-                        region: "Minas Gerais"
-                    },
-                    {
-                        companyID: 10005,
-                        companyName: "PASTELÃO",
-                        corporateCode: "15.345.678/0001-40",
-                        webSite: "www.pastelao.com",
-                        email: "contato@pastelao.com",
-                        region: "Rio de Janeiro"
-                    },
-                ]
-                // cria um modelo JSON com as empresas 
-                const oModel = new JSONModel()
-                oModel.loadData('/model/companys.json')
-                // faz a ligação com a ViewXML (Aggregation Binding)
-                this.getView().setModel(oModel, "companyInfo")
+        loadJSON.then((oModel) => {
+          this.getView().setModel(oModel, "companyInfo");
 
-            },
-            // função que chama o modal (botao declarado na view)
-            openModal: function () {
-                this.createModal()
-            },
-
-            // função chamando o Dialog de Cadastro de empresa
-            createModal: function () {
-                const viewId = this.getView().getId()
-
-                if (!this.dialog) {
-                    this.dialog = sap.ui.xmlfragment(viewId, "com.lab2dev.btpexperience.view.fragments.Dialog", this)
-                    this.getView().addDependent(this.dialog)
-                }
-                this.dialog.open()
-            },
-            onCloseDialog: function () {
-                this.dialog.close()
-            },
-            // SearchField adicionado 
-            onSearch: function (oEvent) {
-                // add filter for search
-                var aFilters = []
-                var sQuery = oEvent.getSource().getValue()
-                if (sQuery && sQuery.length > 0) {
-                    var filter = new Filter("companyName", FilterOperator.Contains, sQuery)
-                    aFilters.push(filter)
-                }
-    
-                // update list binding
-                var oList = this.byId("tableID")
-                var oBinding = oList.getBinding("rows")
-                oBinding.filter(aFilters)
-            },
+          const oViewDetailsModel = new JSONModel({
+            totalCompanyCount: oModel.getData().length,
+          });
+          this.getView().setModel(oViewDetailsModel, "viewDetails");
         });
+      },
+      loadData: async function () {
+        
+        const oModel = new JSONModel();
+
+        await oModel.loadData("/model/companys.json");
+
+        return oModel;
+      },
+      // função chamando o Dialog de Cadastro de empresa
+      openModal: function () {
+        const viewId = this.getView().getId();
+
+        if (!this.dialog) {
+          this.dialog = sap.ui.xmlfragment(
+            viewId,
+            "com.lab2dev.btpexperience.view.fragments.Dialog",
+            this
+          );
+
+          this.getView().addDependent(this.dialog);
+        }
+        // cria uma nova instancia JSONModel vazia que vai receber os novos dados do formulario
+        const oDataModel = new JSONModel({
+          companyName: "",
+          corporateCode: "",
+          webSite: "",
+          email: "",
+          region: "",
+          select: "",
+        });
+        //
+        this.getView().setModel(oDataModel, "formData");
+        this.dialog.open();
+      },
+      // função p/ salvar os novos dados na tabela
+      onSendFormData: function () {
+        const formData = this.getView().getModel("formData").getData();
+        const companyInfoList = this.getView()
+          .getModel("companyInfo")
+          .getData();
+
+        const aCompanies = [
+          ...companyInfoList,
+          {
+            ...formData,
+            companyID:
+              companyInfoList[companyInfoList.length - 1].companyID + 1,
+          },
+        ];
+
+        this.getView().getModel("companyInfo").setData(aCompanies);
+
+        this.getView()
+          .getModel("viewDetails")
+          .setProperty("/totalCompanyCount", aCompanies.length);
+
+        this.dialog.close();
+      },
+      onCancelAddForm: function () {
+        this.dialog.close();
+      },
+      // SearchField adicionado
+      onSearch: function (oEvent) {
+        var aFilters = [];
+        var sQuery = oEvent.getSource().getValue();
+        if (sQuery && sQuery.length > 0) {
+          var filter = new Filter({
+            filters: [
+              new Filter("companyName", FilterOperator.Contains, sQuery),
+              new Filter("region", FilterOperator.Contains, sQuery),
+            ],
+          });
+          aFilters.push(filter);
+        }
+
+        var oTable = this.byId("tableID");
+        var oBinding = oTable.getBinding("rows");
+        oBinding.filter(aFilters);
+
+        const oViewDetailsModel = this.getView().getModel("viewDetails");
+        oViewDetailsModel.setProperty(
+          "/totalCompanyCount",
+          oBinding.getLength()
+        );
+        debugger;
+      },
+      // Navegação para a pagina de detalhes
+      navCompanyDetail: function (oEvent) {
+        // Origem do evento (Item clicado da tabela)
+        const source = oEvent.getSource();
+        //contexto do item da Tabela (Nome do model)
+        const context = source.getBindingContext("companyInfo");
+        // Index do item da Tabela
+        const path = context.getPath();
+        // Acesso ao Objeto do item da tabela pelo path
+        const company = context.getObject(path);
+        // Acesso ao ID da companhia
+        const companyId = company.companyID;
+        // Acesso ao Component da App.controller
+        const oComponent = this.getOwnerComponent();
+        // Acesso ao Router
+        const oRouter = oComponent.getRouter();
+        // Navegação para a rota CompanyDetail
+        oRouter.navTo("CompanyDetail", {
+          companyId: companyId,
+        });
+      },
     });
+  }
+);
